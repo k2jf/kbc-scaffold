f9ab8450f49fb3a8083d6ea123ee654fd265f9f5
zhanghao <1891934@qq.com>
2019-03-08 15:03:32 +0800

    添加kmx代理controller

---
+++ src/main/java/com/k2data/kbc/kmx/KmxProxyController.java
@@ -0,0 +1,22 @@
+package com.k2data.kbc.kmx;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhanghao on 2019/3/8.
+ * 用于转发来自前端的KMX请求
+ */
+@RestController
+public class KmxProxyController {
+
+    @Value("${kbc.kmx.url}")
+    private String kmxUrl;
+
+    @RequestMapping(value = "/kmx")
+    public String hello(@PathVariable String path) {
+        return "access " + kmxUrl + "/" + path;
+    }
+}
