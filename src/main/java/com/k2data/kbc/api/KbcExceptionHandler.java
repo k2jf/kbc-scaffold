15af4804c001a20fcffe5f8eddb85ac50c51edf9
zhanghao <1891934@qq.com>
2019-03-12 15:19:29 +0800

    加入全局异常处理

---
+++ src/main/java/com/k2data/kbc/api/KbcExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.k2data.kbc.api;
+
+import org.apache.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by zhanghao on 2019/3/12.
+ * 全局异常处理配置
+ */
+@ControllerAdvice
+public class KbcExceptionHandler {
+
+    @ExceptionHandler(KbcException.class)
+    @ResponseBody
+    public KbcResponse kbcExceptionHandler(HttpServletResponse httpResp, KbcException e) {
+        KbcResponse kbcResp = new KbcResponse(e.getMessage());
+        httpResp.setStatus(e.getHttpCode());
+        return kbcResp;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public KbcResponse generalException(HttpServletResponse httpResp, Exception e) {
+        KbcResponse kbcResp = new KbcResponse(e.getMessage());
+        httpResp.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR); //500
+        return kbcResp;
+    }
+}
