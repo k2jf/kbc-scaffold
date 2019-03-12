e324e86d15c66ba1d785d101d8346db7b42753ba
zhanghao <1891934@qq.com>
2019-03-12 15:24:11 +0800

    添加api注释

--- src/main/java/com/k2data/kbc/Application.java
+++ src/main/java/com/k2data/kbc/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Api
+@Api("欢迎")
 @EnableSwagger2
 @RestController
 @SpringBootApplication
