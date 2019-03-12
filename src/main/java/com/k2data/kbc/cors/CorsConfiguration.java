48397268038fa1fe2f1e0e8b35f8f781f7882886
zhanghao <1891934@qq.com>
2019-03-12 19:42:30 +0800

    添加cors的filter实现

--- src/main/java/com/k2data/kbc/cors/CorsConfiguration.java
+++ src/main/java/com/k2data/kbc/cors/CorsConfiguration.java
@@ -7,10 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.context.request.RequestContextListener;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -21,74 +18,18 @@ import java.util.List;
 
 /**
  * 支持跨域请求的配置项
+ * TODO: 未能生效，暂时使用CorsFilter
  */
-@Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*")
-                .allowedHeaders("Accept", "Content-Type", "Origin", "Authorization", "X-Auth-Token")
-                .exposedHeaders("X-Auth-Token", "Authorization")
-                .allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS");
-    }
+//@Configuration
+public class CorsConfiguration {
 
     @Bean
-    public FilterRegistrationBean getMyFilter() {
-        MyFilter filter = new MyFilter();
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(filter);
-        List<String> urlPatterns = new ArrayList<String>();
-        urlPatterns.add("");//拦截路径，可以添加多个
-        registrationBean.setUrlPatterns(urlPatterns);
-        registrationBean.setOrder(1);
-        return registrationBean;
-    }
-
-    @Bean
-    public RequestContextListener requestContextListener() {
-        return new RequestContextListener();
-    }
-
-    class MyFilter implements Filter {
-
-        protected String allowMethods;
-        protected String allowOrigin;
-        private static final String DEFAULT_ALLOW_HEADERS = "Content-Type,X-Requested-With";
-        /**
-         * support custom headers
-         */
-        protected String allowHeaders;
-
-        @Override
-        public void init(FilterConfig fConfig) throws ServletException {
-            this.allowMethods = fConfig.getInitParameter("Access-Control-Allow-Methods");
-            this.allowOrigin = fConfig.getInitParameter("Access-Control-Allow-Origin");
-            this.allowHeaders = fConfig.getInitParameter("Access-Control-Allow-Headers");
-            if (this.allowHeaders == null) {
-                this.allowHeaders = DEFAULT_ALLOW_HEADERS;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
             }
-        }
-
-        @Override
-        public void doFilter(ServletRequest request, ServletResponse response,
-                             FilterChain chain) throws IOException, ServletException {
-            HttpServletResponse hResponse = (HttpServletResponse) response;
-            hResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-            hResponse.setHeader("Access-Control-Allow-Origin", ((HttpServletRequest) request).getHeader("origin"));
-            hResponse.setHeader("Access-Control-Allow-Credentials", "true");
-            hResponse.setHeader("Access-Control-Allow-Headers", this.allowHeaders);
-            hResponse.setCharacterEncoding("UTF-8");
-            chain.doFilter(request, hResponse);
-
-        }
-
-        @Override
-        public void destroy() {
-            this.allowMethods = null;
-            this.allowOrigin = null;
-            this.allowHeaders = null;
-        }
+        };
     }
-
 }
---
+++ src/main/java/com/k2data/kbc/cors/CorsFilter.java
@@ -0,0 +1,56 @@
+package com.k2data.kbc.cors;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by zhanghao on 2019/3/12.
+ * TODO: @CrossOrigin注解未能生效(https://spring.io/guides/gs/rest-service-cors/)，暂时使用filter方案
+ * <p>
+ * https://www.cnblogs.com/yoyotl/p/7090996.html
+ */
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) throws IOException, ServletException {
+
+        //处理逻辑放在chain.doFilter之后进行
+        chain.doFilter(req, res);
+
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        //加条件判断以免重复添加同名header（考虑kmx反向代理的例子）
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+        }
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.setHeader("Access-Control-Allow-Methods",
+                    "POST, GET, OPTIONS, PUT, DELETE");
+        }
+        if (response.getHeader("Access-Control-Allow-Credentials") == null) {
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+        }
+        if (response.getHeader("Access-Control-Max-Age") == null) {
+            response.setHeader("Access-Control-Max-Age", "3600");
+        }
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.setHeader("Access-Control-Allow-Headers",
+                    "Content-Type, x-requested-with, X-Custom-Header, Authorization");
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+}
